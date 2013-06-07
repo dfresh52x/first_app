2dc77999f56e4302c9da205fa0b5e502db400f3e
U-Fletch-PC\Fletch <Fletch@Fletch-PC.(none)>
2013-06-07 18:04:44 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'c653fb014cd4f44a4318b01e4afb34e9dccf3a77173f27a4c4777b7a7ae10d175a4663c2ff6f8e981a8dc0c7a7c67cab5098e5a8a34cc0a315b7d9ace71c843e'
